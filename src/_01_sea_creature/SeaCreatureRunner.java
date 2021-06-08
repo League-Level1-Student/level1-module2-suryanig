cd65b23b8a1c917e0699bc47308367b93a574a70
suryanig <67238881+suryanig@users.noreply.github.com>
2021-06-07 18:26:30 -0700

    I finished sea creature and smurf today

--- src/_01_sea_creature/SeaCreatureRunner.java
+++ src/_01_sea_creature/SeaCreatureRunner.java
@@ -1,5 +1,19 @@
 package _01_sea_creature;
 
 public class SeaCreatureRunner {
-
+	public static void main(String[] args) {
+		SeaCreature spongebob = new SeaCreature("Spongebob");
+		spongebob.eat();
+		spongebob.laugh();
+		
+		SeaCreature patrick = new SeaCreature("Patrick");
+		System.out.println(patrick.getName());
+		patrick.eat();
+		patrick.laugh();
+		
+		SeaCreature squidward = new SeaCreature("Squidward");
+		System.out.println(squidward.getName());
+		squidward.eat();
+		squidward.laugh();
+	}
 }
---
+++ src/_02_smurf/SmurfRunner.java
@@ -0,0 +1,21 @@
+package _02_smurf;
+
+public class SmurfRunner {
+	public static void main(String[] args) {
+		Smurf handySmurf = new Smurf("Handy Smurf");
+		handySmurf.eat();
+		System.out.println(handySmurf.getName());
+		
+		Smurf papaSmurf = new Smurf("Papa Smurf");
+		System.out.println(papaSmurf.getName());
+		System.out.println(papaSmurf.getHatColor());
+		System.out.println(papaSmurf.isGirlOrBoy());
+		
+		Smurf smurfette = new Smurf("Smurfette");
+		System.out.println(smurfette.getName());
+		System.out.println(smurfette.getHatColor());
+		System.out.println(smurfette.isGirlOrBoy());
+		
+		
+	}
+}
