cf2503bfa3255663155303ceb5a78a0489727cff
suryanig <67238881+suryanig@users.noreply.github.com>
2021-06-23 16:58:16 -0700

    I started tea party

---
+++ src/_06_tea_party/TeaParty.java
@@ -0,0 +1,15 @@
+package _06_tea_party;
+
+public class TeaParty {
+	/**
+ 	 * Jane Austen is a woman, so say “Hello Ms. Austen”. 
+ 	 * George Orwell is a man, so say “Hello Mr. Orwell”. 
+ 	 * Isaac Newton was knighted, so say "Hello Sir Isaac Newton".
+ 	 **/
+
+	public String welcome(String name, boolean b, boolean c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
